e7a359ebfa47b2442c99f5fc7a6189ecd73c232a
のぞみ_44 <nozomi.muscle.10.02@gmail.com>
2023-12-26 13:20:58 +0900

    :zap: Add: sport_contentカラム 最大文字数validation追加

--- app/models/training_record.rb
+++ app/models/training_record.rb
@@ -1,6 +1,8 @@
 class TrainingRecord < ApplicationRecord
   belongs_to :user
 
+  validates :sport_content, length: { maximum: 100 }
+
   def self.check_report?(user)
     last_report = user.training_records.order(start_time: :desc).first
     last_report && last_report.start_time.to_date == Date.today
