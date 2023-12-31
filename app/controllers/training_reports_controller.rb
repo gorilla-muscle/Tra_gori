06ecb50c8866ab82c9afc2fc9c8f4185a546bc4a
のぞみ_44 <nozomi.muscle.10.02@gmail.com>
2023-12-31 20:48:01 +0900

    :lipstick: Feat: 未報告時の画面遷移制限を追加

--- app/controllers/training_reports_controller.rb
+++ app/controllers/training_reports_controller.rb
@@ -1,5 +1,15 @@
 class TrainingReportsController < ApplicationController
   before_action :require_login
-  
+  before_action :report_conf
+
   def index; end
+
+  private
+
+  def report_conf
+    unless TrainingRecord.check_report?(current_user)
+      flash[:warning] = t(".not_report")
+      redirect_to training_records_path
+    end
+  end
 end
