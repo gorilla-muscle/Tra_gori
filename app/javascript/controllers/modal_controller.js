6748caa1f5ae344adebaf30daf683acbc54cc4bb
のぞみ_44 <nozomi.muscle.10.02@gmail.com>
2024-01-16 12:44:09 +0900

    :sparkles: #56 feat: OpenAI生成中のモーダル画面実装

---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="modal"
+export default class extends Controller {
+  connect() {
+    document.addEventListener('turbo:submit-start', () => {
+      this.showModal();
+    });
+
+    document.addEventListener('turbo:submit-end', () => {
+      this.hideModal();
+    });
+  }
+
+  showModal() {
+    document.getElementById('modal').classList.remove('hidden');
+  }
+
+  hideModal() {
+    document.getElementById('modal').classList.add('hidden');
+  }
+}
