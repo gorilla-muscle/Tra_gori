98ccd1fd387539ede7b53ef207d396fd4d57235f
のぞみ_44 <nozomi.muscle.10.02@gmail.com>
2024-01-18 18:17:04 +0900

    js関連修正

--- app/javascript/title/title_updater.js
+++
@@ -1,6 +0,0 @@
-// document.addEventListener("turbo:load", () => {
-//   const titleElement = document.querySelector("meta[name='turbo-title']");
-//   if (titleElement) {
-//     document.title = titleElement.content;
-//   }
-// });
