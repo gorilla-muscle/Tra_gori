a13e61831d70d76ff0ed660d0f55f589d9aede09
のぞみ_44 <nozomi.muscle.10.02@gmail.com>
2024-01-20 12:12:37 +0900

    :wrench: Fix: ActionMailer設定の修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,20 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = Settings.default_url_options.to_h
-  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    #gmail利用時はaddress,domain,portは下記で固定
+    address:"smtp.gmail.com",
+    domain: 'gmail.com',
+    port:587,
+    #gmailのユーザアカウント
+    user_name: Rails.application.credentials.gmail[:user_name],
+    #gmail２段階認証回避のためのアプリケーションでの利用パスワード
+    password: Rails.application.credentials.gmail[:password],
+    #パスワードをBase64でエンコード
+    authentication: :login
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
