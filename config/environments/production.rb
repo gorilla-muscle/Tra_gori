c1baef436cc1a54a8703b0a8a7ca8f9e70cf0d18
のぞみ_44 <nozomi.muscle.10.02@gmail.com>
2024-01-21 14:32:02 +0900

    :wrench: Fix: credentials修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,19 +65,21 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = Settings.default_url_options.to_h
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    #gmail利用時はaddress,domain,portは下記で固定
-    address:"smtp.gmail.com",
-    domain: 'gmail.com',
-    port:587,
-    #gmailのユーザアカウント
-    user_name: Rails.application.credentials.gmail[:gmail_name],
-    #gmail２段階認証回避のためのアプリケーションでの利用パスワード
-    password: Rails.application.credentials.gmail[:gmail_password],
-    #パスワードをBase64でエンコード
-    authentication: :login,
-    enable_starttls: true
-  }
+  if Rails.application.credentials.gmail
+    config.action_mailer.smtp_settings = {
+      #gmail利用時はaddress,domain,portは下記で固定
+      address:"smtp.gmail.com",
+      domain: 'gmail.com',
+      port:587,
+      #gmailのユーザアカウント
+      user_name: Rails.application.credentials.gmail[:gmail_name],
+      #gmail２段階認証回避のためのアプリケーションでの利用パスワード
+      password: Rails.application.credentials.gmail[:gmail_password],
+      #パスワードをBase64でエンコード
+      authentication: :login,
+      enable_starttls: true
+    }
+  end
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
